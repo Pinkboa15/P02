73271de21e605749a83bb3dfd881d76c162be9e3
Andrea <aag170001@utdallas.edu>
2020-10-27 19:31:47 -0500

    Added health pickup

---
+++ Assets/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private PlayerHealth healthBonus;
+    private int healamount = 20;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        healthBonus = player.GetComponent<PlayerHealth>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        healthBonus.HealPlayer(healamount);
+        Destroy(this.gameObject);
+    }
+}
